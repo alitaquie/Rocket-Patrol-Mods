4cdd036651ba37e179952c66114b065a1c56145b
ali taquie <alitaquie51@gmail.com>
2024-02-01 18:50:54 -0800

    final comments and mods added ^-^

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
+/*
+Name: Mohammad Taquie
+Title: Rocket Patrol Mods
+Time: 10 hours
+
+Mods implemented:
+---------------------------------------------------------------------------------------
+1-point tier:
+
+    - Add your own (copyright-free) looping background music to the Play scene (1)
+    - Allow the player to control the Rocket after it's fired (1)
+
+    total: 2
+
+3-point tier: 
+    - Display the time remaining (in seconds) on the screen (3)
+
+    total: 3
+
+5-point tier:
+    - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
+    -Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+    -Implement mouse control for player movement and left mouse click to fire (5)
+
+    total: 15
+
+Overall score: 20
+---------------------------------------------------------------------------------------
+citations:
+
+ https://rexrainbow.github.io/phaser3-rex-notes/docs/site/timer/
+
+ spaceship art done on Piskel 
+
+
+*/
+
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
